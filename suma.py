5c91b60db5932f9bfd6bf79f1dd6c963d463d15c
Zerok74 <69864508+Zerok74@users.noreply.github.com>
2025-06-18 20:19:04 -0400

    Add files via upload

---
+++ suma.py
@@ -0,0 +1,8 @@
+#funcion simple de suma para github
+print("===suma de dos numeros===")
+def sumar(a, b):
+    return a + b
+num1 = int(input("Ingresa el primer número: "))
+num2 = int(input("Ingresa el segundo número: "))
+resultado = sumar(num1, num2)
+print("La suma es:", resultado)
